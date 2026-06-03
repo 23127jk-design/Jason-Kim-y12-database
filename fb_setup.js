ed11781408a16d21fa2a1deab103e8999d4c4d23
23127jk <23127jk@hvhs.school.nz>
2026-06-04 10:59:45 +1200

    starting project

---
+++ fb_setup.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuiRdlAIgcuos4cms93TdjfxdgeklOF4Y",
+  authDomain: "jason-kim-12comp-project.firebaseapp.com",
+  databaseURL: "https://jason-kim-12comp-project-default-rtdb.firebaseio.com",
+  projectId: "jason-kim-12comp-project",
+  storageBucket: "jason-kim-12comp-project.firebasestorage.app",
+  messagingSenderId: "672626092873",
+  appId: "1:672626092873:web:bf745016a1c737575f0898"
+};
+// Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+    console.log("Firebase initialize finished:");
+  console.log(firebase);
